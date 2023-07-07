47b280a05afdcaf2bcc1328715cb0493f9b41df4
JeonSeongJin <ayclub8866@naver.com>
2023-07-07 14:10:52 +0900

    refactor: 비동기 처리에 async/await 사용 , 'fetchPosts' 메소드에서 프로미스 체이닝('then/catch')을 사용하고 있었음. 가독성을 좋게 하기 위해 async/await 로 변경

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -13,7 +13,11 @@
       </thead>
       <tbody>
         <!-- 글 목록 아이템 반복 표시 -->
-        <tr v-for="post in paginatedPosts" :key="post.id" @click="handlePostClick(post)">
+        <tr
+          v-for="post in paginatedPosts"
+          :key="post.id"
+          @click="handlePostClick(post)"
+        >
           <td>{{ post.id }}</td>
           <td>{{ post.title }}</td>
           <td>{{ post.author }}</td>
@@ -22,30 +26,49 @@
       </tbody>
     </table>
     <!-- Post Detail Modal -->
- <post-detail :post="selectedPost" :showModal="showPostDetail" v-if="showPostDetail" @close="showPostDetail = false" @postDeleted="fetchPosts" />
-<nav>
-  <ul class="pagination">
-    <!-- 이전 버튼 -->
-    <li class="page-item" :class="{ 'disabled': currentPage <= 10 }">
-      <button class="page-link" @click="prevPage" :disabled="currentPage <= 10">이전</button>
-    </li>
-    <!-- 페이지 버튼 -->
-    <li class="page-item" v-for="pageNumber in visiblePageNumbers" :key="pageNumber" :class="{ 'active': currentPage === pageNumber }">
-      <button class="page-link" @click="goToPage(pageNumber)">{{ pageNumber }}</button>
-    </li>
-    <!-- 다음 버튼 -->
-    <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
-      <button class="page-link" @click="nextPage">다음</button>
-    </li>
-  </ul>
-</nav>
-<div class="row">
-    <div class="col text-end">
-      <router-link to="/write" v-slot="{navigate}">
-       <button class="btn btn-primary" @click="navigate">글 쓰기</button>
-       </router-link>
+    <post-detail
+      :post="selectedPost"
+      :showModal="showPostDetail"
+      v-if="showPostDetail"
+      @close="showPostDetail = false"
+      @postDeleted="fetchPosts"
+    />
+    <nav>
+      <ul class="pagination">
+        <!-- 이전 버튼 -->
+        <li class="page-item" :class="{ disabled: currentPage <= 10 }">
+          <button
+            class="page-link"
+            @click="prevPage"
+            :disabled="currentPage <= 10"
+          >
+            이전
+          </button>
+        </li>
+        <!-- 페이지 버튼 -->
+        <li
+          class="page-item"
+          v-for="pageNumber in visiblePageNumbers"
+          :key="pageNumber"
+          :class="{ active: currentPage === pageNumber }"
+        >
+          <button class="page-link" @click="goToPage(pageNumber)">
+            {{ pageNumber }}
+          </button>
+        </li>
+        <!-- 다음 버튼 -->
+        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+          <button class="page-link" @click="nextPage">다음</button>
+        </li>
+      </ul>
+    </nav>
+    <div class="row">
+      <div class="col text-end">
+        <router-link to="/write" v-slot="{ navigate }">
+          <button class="btn btn-primary" @click="navigate">글 쓰기</button>
+        </router-link>
+      </div>
     </div>
-</div>
   </div>
 </template>
 <style>
@@ -83,13 +106,12 @@
   margin-top: 10px;
 }
 
-
 .pagination button {
   margin: 0 5px;
 }
 </style>
 <script>
-import PostDetail from './PostDetail.vue';
+import PostDetail from './PostDetail.vue'
 
 export default {
   data() {
@@ -99,82 +121,79 @@ export default {
       pageSize: 10, // 한 페이지에 보여줄 게시글 수
       showPostDetail: false, // Post Detail Modal show/hide
       selectedPost: null, // 현재 선택된 게시물을 저장
-    };
+    }
   },
   computed: {
     totalPages() {
-      return Math.ceil(this.posts.length / this.pageSize);
+      return Math.ceil(this.posts.length / this.pageSize)
     },
     visiblePageNumbers() {
-      const currentPageGroup = Math.ceil(this.currentPage / 10);
-      const startPage = (currentPageGroup - 1) * 10 + 1;
-      const endPage = Math.min(this.totalPages, currentPageGroup * 10);
+      const currentPageGroup = Math.ceil(this.currentPage / 10)
+      const startPage = (currentPageGroup - 1) * 10 + 1
+      const endPage = Math.min(this.totalPages, currentPageGroup * 10)
       return Array(endPage - startPage + 1)
         .fill()
         .map((_, index) => startPage + index)
-        .filter((pageNumber) => pageNumber > 0); // 1보다 작은 페이지 제외
+        .filter((pageNumber) => pageNumber > 0) // 1보다 작은 페이지 제외
     },
     paginatedPosts() {
-      const startIndex = (this.currentPage - 1) * this.pageSize;
-      const endIndex = startIndex + this.pageSize;
-      return this.posts.slice(startIndex, endIndex);
+      const startIndex = (this.currentPage - 1) * this.pageSize
+      const endIndex = startIndex + this.pageSize
+      return this.posts.slice(startIndex, endIndex)
     },
     isAllDataVisible() {
-    return this.currentPage === 1 && this.totalPages <= 10;
+      return this.currentPage === 1 && this.totalPages <= 10
     },
     hasNextPage() {
-      return this.currentPage < this.totalPages;
+      return this.currentPage < this.totalPages
     },
-
   },
   methods: {
     formatDate(dateString) {
-      const options = { year: "numeric", month: "long", day: "numeric" };
-      const date = new Date(dateString);
-      return date.toLocaleDateString(undefined, options);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      const date = new Date(dateString)
+      return date.toLocaleDateString(undefined, options)
     },
     handlePostClick(post) {
       // 클릭한 글에 대한 동작 처리
-      console.log("Clicked post:", post);
-      this.selectedPost = post; // 선택된 게시물 설정
-      this.showPostDetail = true; // 모달 보이기
+      console.log('Clicked post:', post)
+      this.selectedPost = post // 선택된 게시물 설정
+      this.showPostDetail = true // 모달 보이기
     },
-    fetchPosts() {
-      // 서버에서 게시글 목록을 가져오는 메소드
-      fetch('http://localhost:3001/posts')
-        .then(response => response.json())
-        .then(data => {
-          this.posts = data; // 가져온 게시글 목록을 데이터에 할당
-        })
-        .catch(error => {
-          console.log('Error fetching posts:', error);
-        });
+    async fetchPosts() {
+      try {
+        const response = await fetch('http://localhost:3001/posts')
+        const data = await response.json()
+        this.posts = data
+      } catch (error) {
+        console.log('Error fetching posts:', error)
+      }
     },
     prevPage() {
       if (this.currentPage > 1) {
-        this.currentPage -= 10;
+        this.currentPage -= 10
       }
     },
     nextPage() {
       if (this.hasNextPage) {
-        this.currentPage += 10;
+        this.currentPage += 10
         if (this.currentPage > this.totalPages) {
-          this.currentPage = this.totalPages;
+          this.currentPage = this.totalPages
         }
       }
     },
     goToPage(pageNumber) {
-      this.currentPage = pageNumber;
+      this.currentPage = pageNumber
     },
-    created(){
-      this.fetchPosts();
-    }
+    created() {
+      this.fetchPosts()
+    },
   },
   mounted() {
-    this.fetchPosts(); // 컴포넌트가 마운트되면 게시글 목록을 가져오도록 호출
+    this.fetchPosts() // 컴포넌트가 마운트되면 게시글 목록을 가져오도록 호출
   },
-  components:{
-    PostDetail
-  }
-};
+  components: {
+    PostDetail,
+  },
+}
 </script>
